fc50d5efbc2824c62b514000ad9865e3df0617f8
Saif-Uddin0 <mahirr.846@gmail.com>
2026-04-19 00:25:08 +0600

    upgrade: update login API endpoint to test11.fireai.agency

--- src/pages/Authintication/Login.jsx
+++ src/pages/Authintication/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     try {
 
       const res = await axios.post(
-        "https://test22.fireai.agency/auth/login/",
+        "https://test11.fireai.agency/auth/login/",
         data
       );
 
